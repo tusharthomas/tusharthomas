2e017374132f42a2ed0c93485255f3fcacfffe68
Tushar Thomas <tushar175@gmail.com>
2022-08-20 12:35:20 -0400

    Modifies logic to align output

--- Java/Fahr2Celsius/F2C.java
+++ Java/Fahr2Celsius/F2C.java
@@ -4,7 +4,7 @@ public class F2C {
     public static void main(String[] args) {
         int f = getInputInt("Enter a Fahrenheit value: ");
         double c = (5.0/9) * (f - 32);
-        System.out.println("Celsius: " + c);
+        System.out.printf("%-25s %.1f\n","Celsius:", c);
     }
 
     public static int getInputInt(String prompt) {
